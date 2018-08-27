ff37a741492e9a62ff16a6964d7c22f6b7b444d1
lsqboy <34208535@qq.com>
2018-08-27 15:22:08 +0800

    新增推荐，热榜

--- lib/home/hot/hot.dart
+++ lib/home/hot/hot.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_zhifu_ui/global_config.dart';
+import 'package:flutter_zhifu_ui/home/model/question.dart';
 
 class Hot extends StatefulWidget {
   @override
@@ -10,17 +12,114 @@ class Hot extends StatefulWidget {
 class _Hot extends State<Hot> {
   @override
   Widget build(BuildContext context) {
+    Widget hotCard(Question question) {
+      return new Container(
+        decoration: new BoxDecoration(
+            color: GlobalConfig.cardBackgroundColor,
+            border: new BorderDirectional(
+                bottom: new BorderSide(
+                    color: GlobalConfig.dark == true
+                        ? Colors.white12
+                        : Colors.black12,
+                    width: 1.0))),
+        child: new FlatButton(
+            onPressed: null,
+            child: new Container(
+              padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+              child: new Row(
+                children: <Widget>[
+                  new Expanded(
+                    child: new Column(
+                      children: <Widget>[
+                        new Container(
+                          child: new Text(
+                            question.order,
+                            style: new TextStyle(
+                              color: question.order.compareTo("03") <= 0
+                                  ? Colors.red
+                                  : Colors.yellow,
+                              fontSize: 18.0,
+                            ),
+                          ),
+                          alignment: Alignment.topLeft,
+                        ),
+                        question.rise != null
+                            ? new Row(
+                                children: <Widget>[
+                                  new Icon(
+                                    Icons.arrow_upward,
+                                    color: Colors.red,
+                                    size: 10.0,
+                                  ),
+                                  new Text(
+                                    question.rise,
+                                    style: new TextStyle(
+                                        color: Colors.red, fontSize: 10.0),
+                                  )
+                                ],
+                              )
+                            : new Container()
+                      ],
+                    ),
+                    flex: 1,
+                  ),
+                  new Expanded(
+                    child: new Column(
+                      children: <Widget>[
+                        new Container(
+                          child: new Text(question.title,
+                          style: new TextStyle(color: GlobalConfig.dark == true ? Colors.white70 : Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0,height: 1.1),),
+                          padding: const EdgeInsets.only(bottom: 10.0,right: 4.0),
+                          alignment: Alignment.topLeft,
+                        ),
+                        question.mark !=null?new Container(
+                          child: new Text(question.mark,style: new TextStyle(color: GlobalConfig.fontColor)),
+                          padding: const EdgeInsets.only(bottom: 8.0,right: 4.0),
+                          alignment: Alignment.topLeft,
+                        ):new Container(),
+                        new Container(
+                          child: new Text(question.hotNum,style: new TextStyle(color: GlobalConfig.fontColor),),
+                          alignment: Alignment.topLeft,
+                        )
+
+                      ],
+                    ),
+                    flex: 6,
+                  ),
+                  new Expanded(
+                    child: new AspectRatio(aspectRatio: 3.0/2.0,
+                    child: new Container(
+                      foregroundDecoration: new BoxDecoration(
+                        image: new DecorationImage(
+                          image: new NetworkImage(question.imgUrl),
+                          centerSlice: new Rect.fromLTRB(270.0, 180.0, 1360.0, 730.0)
+                        ),
+                        borderRadius: const BorderRadius.all(const Radius.circular(6.0))
+                      ),
+                    ),),
+                    flex: 3,
+                  ),
+                ],
+              ),
+            )),
+      );
+    }
+
     return new SingleChildScrollView(
       child: new Container(
-        margin: const EdgeInsets.only(top: 20.0),
+        margin: const EdgeInsets.only(top: 5.0),
         child: new Column(
           children: <Widget>[
-            new Text('hot1'),
-            new Container(margin: const EdgeInsets.only(top: 20.0),),
-            new Text('hot2'),
-            new Container(margin: const EdgeInsets.only(top: 20.0),),
-            new Text('hot3'),
-            new Container(margin: const EdgeInsets.only(top: 20.0),),
+            new Container(
+              margin: const EdgeInsets.all(5.0),
+            ),
+            hotCard(questionList[0]),
+            hotCard(questionList[1]),
+            hotCard(questionList[2]),
+            hotCard(questionList[3]),
+            hotCard(questionList[4]),
           ],
         ),
       ),
--- lib/home/recommend/recommend.dart
+++ lib/home/recommend/recommend.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_zhifu_ui/global_config.dart';
+import 'package:flutter_zhifu_ui/home/model/article.dart';
+import 'package:flutter_zhifu_ui/home/model/question.dart';
+import 'package:flutter_zhifu_ui/my/my_page.dart';
 
 class Recommend extends StatefulWidget {
   @override
@@ -10,17 +14,135 @@ class Recommend extends StatefulWidget {
 class _Recommend extends State<Recommend> {
   @override
   Widget build(BuildContext context) {
+    Widget wordsCard(Article article) {
+      Widget markWidget;
+      if (article.imgUrl == null) {
+        markWidget = new Text(
+          article.mark,
+          style: new TextStyle(height: 1.3, color: GlobalConfig.fontColor),
+        );
+      } else {
+        markWidget = new Row(
+          children: <Widget>[
+            new Expanded(
+              child: new Container(
+                child: new Text(
+                  article.title,
+                  style:
+                  new TextStyle(height: 1.3, color: GlobalConfig.fontColor),
+                ),
+              ),
+              flex: 2,
+            ),
+            new Expanded(
+                child: new AspectRatio(
+                  aspectRatio: 3.0 / 2.0,
+                  child: new Container(
+                    foregroundDecoration: new BoxDecoration(
+                        image: new DecorationImage(
+                            image: new NetworkImage(article.imgUrl),
+                            centerSlice:
+                            new Rect.fromLTRB(270.0, 180.0, 1360.0, 730.0)),
+                        borderRadius:
+                        const BorderRadius.all(const Radius.circular(6.0))),
+                  ),
+                ),
+                flex: 1)
+          ],
+        );
+      }
+      return new Container(
+        color: GlobalConfig.cardBackgroundColor,
+        margin: const EdgeInsets.only(top: 5.0, bottom: 5.0),
+        child: new FlatButton(
+            onPressed: () {
+              Navigator
+                  .of(context)
+                  .push(new MaterialPageRoute(builder: (context) {return new MyPage();}));
+            },
+            child: new Column(
+              children: <Widget>[
+                new Container(
+                  child: new Row(
+                    children: <Widget>[
+                      new Container(
+                        child: new CircleAvatar(
+                          backgroundImage: new NetworkImage(article.headUrl),
+                          radius: 11.0,
+                        ),
+                      ),
+                      new Text(
+                        "  " +
+                            article.user +
+                            " " +
+                            article.action +
+                            " · " +
+                            article.time,
+                        style: new TextStyle(color: GlobalConfig.fontColor),
+                      )
+                    ],
+                  ),
+                  padding: const EdgeInsets.only(top: 10.0),
+                ),
+                new Container(
+                  child: new Text(
+                    article.title,
+                    style: new TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                        height: 1.3,
+                        color: GlobalConfig.dark == true
+                            ? Colors.white70
+                            : Colors.black),
+                  ),
+                  margin: new EdgeInsets.only(top: 6.0, bottom: 2.0),
+                  alignment: Alignment.topLeft,
+                ),
+                new Container(
+                  child: markWidget,
+                  margin: new EdgeInsets.only(top: 6.0),
+                  alignment: Alignment.topLeft,
+                ),
+                new Container(
+                    child: new Row(
+                      children: <Widget>[
+                        new Expanded(
+                            child: new Text(
+                                article.agreeNum.toString() +
+                                    " 赞同 · " +
+                                    article.commentNum.toString() +
+                                    "评论",
+                                style: new TextStyle(color: GlobalConfig.fontColor))),
+                        new PopupMenuButton(
+                            itemBuilder: (BuildContext context) =>
+                            <PopupMenuItem<String>>[
+                              new PopupMenuItem<String>(
+                                child: new Text('屏蔽这个问题'),
+                                value: '选项一的值',
+                              ),
+                              new PopupMenuItem<String>(
+                                child: new Text('取消关注 learner'),
+                                value: '选项二的值',
+                              ),
+                              new PopupMenuItem<String>(
+                                child: new Text('举报'),
+                                value: '选项三的值',
+                              ),
+                            ])
+                      ],
+                    )),
+              ],
+            )),
+      );
+    }
+
     return new SingleChildScrollView(
       child: new Container(
-        margin: const EdgeInsets.only(top: 20.0),
+        margin: const EdgeInsets.only(top: 5.0),
         child: new Column(
           children: <Widget>[
-            new Text('Recommend1'),
-            new Container(margin: const EdgeInsets.only(top: 20.0),),
-            new Text('Recommend2'),
-            new Container(margin: const EdgeInsets.only(top: 20.0),),
-            new Text('Recommend3'),
-            new Container(margin: const EdgeInsets.only(top: 20.0),),
+            wordsCard(articleList[0]),
+            wordsCard(articleList[1]),
           ],
         ),
       ),
