ed0cf5e58eb8fa3971d8d738cadb33d1e895bcdb
lsqboy <34208535@qq.com>
2018-09-03 17:38:53 +0800

    修改模块名。

--- lib/constant/Strings.dart
+++ lib/constant/Strings.dart
@@ -3,7 +3,7 @@ class Strings {
   static const String INDEX_NAV_HOME = "首页";
   static const String INDEX_NAV_ARTICLE = "玩资讯";
   static const String INDEX_NAV_KNOWLEDGE = "知识体系";
-  static const String INDEX_NAV_NOTICE = "通知";
+  static const String INDEX_NAV_NOTICE = "平台交互";
   static const String INDEX_NAV_MINE = "我的";
   ///  home page
   static const String HOME_NAV_FOLLOW = "关注";
